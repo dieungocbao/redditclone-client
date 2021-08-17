c30e1c242cc53b1733ab2df732028a13eb63e0af
dieungocbao <dieungocbao@gmail.com>
2021-08-17 17:45:10 +0700

    register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,38 +1,50 @@
-import React from 'react'
-import { Formik, Form } from 'formik'
-import { Box, Button } from '@chakra-ui/react'
-import Wrapper from '../components/Wrapper'
-import InputField from '../components/InputField'
+import React from "react"
+import { Formik, Form } from "formik"
+import { Box, Button } from "@chakra-ui/react"
+import Wrapper from "../components/Wrapper"
+import InputField from "../components/InputField"
+import { useRegisterMutation } from "../generated/graphql"
+import { toErrorMap } from "../utils/toErrorMap"
+import { useRouter } from "next/router"
 
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = () => {
+  const router = useRouter()
+  const [, register] = useRegisterMutation()
   return (
-    <Wrapper variant="small">
+    <Wrapper variant='small'>
       <Formik
-        initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values)
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            router.push('/')
+          }
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
             <InputField
-              name="username"
-              placeholder="Username"
-              label="Username"
+              name='username'
+              placeholder='Username'
+              label='Username'
             />
             <Box mt={4}>
               <InputField
-                name="password"
-                placeholder="Password"
-                label="Password"
-                type="password"
+                name='password'
+                placeholder='Password'
+                label='Password'
+                type='password'
               />
             </Box>
             <Button
               mt={4}
-              colorScheme="teal"
+              colorScheme='teal'
               isLoading={isSubmitting}
-              type="submit"
+              type='submit'
             >
               Register
             </Button>
