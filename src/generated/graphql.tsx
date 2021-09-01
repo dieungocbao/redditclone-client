c98d00d79ad4c31af42dc77c85af573f5533d111
dieungocbao <dieungocbao@gmail.com>
2021-09-01 17:30:46 +0700

    load more posts

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -70,6 +70,12 @@ export type MutationChangePasswordArgs = {
   token: Scalars['String'];
 };
 
+export type PaginatedPosts = {
+  __typename?: 'PaginatedPosts';
+  posts: Array<Post>;
+  hasMore: Scalars['Boolean'];
+};
+
 export type Post = {
   __typename?: 'Post';
   _id: Scalars['Int'];
@@ -80,6 +86,7 @@ export type Post = {
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
   textSnippet: Scalars['String'];
+  createdBy: User;
 };
 
 export type PostInput = {
@@ -89,7 +96,7 @@ export type PostInput = {
 
 export type Query = {
   __typename?: 'Query';
-  posts: Array<Post>;
+  posts: PaginatedPosts;
   post?: Maybe<Post>;
   me?: Maybe<User>;
 };
@@ -185,7 +192,7 @@ export type PostsQueryVariables = Exact<{
 }>;
 
 
-export type PostsQuery = { __typename?: 'Query', posts: Array<{ __typename?: 'Post', _id: number, title: string, text: string, points: number, textSnippet: string, createdAt: string }> };
+export type PostsQuery = { __typename?: 'Query', posts: { __typename?: 'PaginatedPosts', hasMore: boolean, posts: Array<{ __typename?: 'Post', _id: number, title: string, text: string, points: number, textSnippet: string, createdAt: string }> } };
 
 export const RegularErrorFragmentDoc = gql`
     fragment RegularError on FieldError {
@@ -292,12 +299,15 @@ export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'q
 export const PostsDocument = gql`
     query Posts($limit: Int!, $cursor: String) {
   posts(limit: $limit, cursor: $cursor) {
-    _id
-    title
-    text
-    points
-    textSnippet
-    createdAt
+    hasMore
+    posts {
+      _id
+      title
+      text
+      points
+      textSnippet
+      createdAt
+    }
   }
 }
     `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withUrqlClient } from 'next-urql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { usePostsQuery } from '../generated/graphql'
 import Layout from '../components/Layout'
 import NextLink from 'next/link'
-import { Box, Link, Text } from '@chakra-ui/layout'
+import { Box, Text } from '@chakra-ui/layout'
 import { Button, Flex, Heading, Stack } from '@chakra-ui/react'
 
 interface PropsIF {}
 
 const Index: React.FC<PropsIF> = () => {
+  const [variables, setVariables] = useState({ limit: 5, cursor: null })
   const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+    variables,
   })
   if (!fetching && !data) {
     return <Box>You got query failed for some reason</Box>
@@ -31,7 +30,7 @@ const Index: React.FC<PropsIF> = () => {
           <Box>Loading...</Box>
         ) : (
           <Stack spacing={8}>
-            {data.posts.map((p) => (
+            {data.posts.posts.map((p) => (
               <Box key={p._id} p={5} shadow="md" borderWidth="1px">
                 <Heading fontSize="xl">{p.title}</Heading>
                 <Text mt={4}>{p.textSnippet}</Text>
@@ -40,9 +39,20 @@ const Index: React.FC<PropsIF> = () => {
           </Stack>
         )}
       </Box>
-      {data ? (
+      {data && data.posts.hasMore ? (
         <Flex>
-          <Button colorScheme="facebook" m="auto" my={8}>
+          <Button
+            colorScheme="facebook"
+            m="auto"
+            my={8}
+            isLoading={fetching}
+            onClick={() =>
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              })
+            }
+          >
             Load more
           </Button>
         </Flex>
